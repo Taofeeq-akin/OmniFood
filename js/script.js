6de765a6abfbdda8dfac953303eeece2f7bf7f7d
Taofeeq akin <taofeeqomoakin18@gmail.com>
2022-11-17 15:36:42 +0100

    Makes mobilr navigation works

--- js/script.js
+++ js/script.js
@@ -1,26 +1,35 @@
 ///////////////////////////////////////////////////////////
+// Making mobile navigation works
+const btnNav = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNav.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+//////////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
-    var flex = document.createElement("div");
-    flex.style.display = "flex";
-    flex.style.flexDirection = "column";
-    flex.style.rowGap = "1px";
-  
-    flex.appendChild(document.createElement("div"));
-    flex.appendChild(document.createElement("div"));
-  
-    document.body.appendChild(flex);
-    var isSupported = flex.scrollHeight === 1;
-    flex.parentNode.removeChild(flex);
-    console.log(isSupported);
-  
-    if (!isSupported) document.body.classList.add("no-flexbox-gap");
-  }
-  checkFlexGap();
-  
-  // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
-  
-  /*
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
+
+// https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
+
+/*
   .no-flexbox-gap .main-nav-list li:not(:last-child) {
     margin-right: 4.8rem;
   }
